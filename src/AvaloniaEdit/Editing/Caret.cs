259cb8510adf368d30062d2c9cbd171f13c029f3
ajkfds <ajkfds2018@gmail.com>
2026-08-31 23:02:09 +0900

    fix carlet size

--- src/AvaloniaEdit/Editing/Caret.cs
+++ src/AvaloniaEdit/Editing/Caret.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
+// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -32,7 +32,7 @@ namespace AvaloniaEdit.Editing
     /// </summary>
     public sealed class Caret
     {
-        private const double CaretWidth = 0.5;
+        private const double CaretWidth = 2;
 
         private readonly TextArea _textArea;
         private readonly TextView _textView;
